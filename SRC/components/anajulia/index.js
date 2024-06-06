9cc973bb05163dd2f21ad14e15e0e413339cf98b
Corrre2 <correacarol1996@gmail.com>
2024-06-06 08:13:32 -0300

    provaddm2

---
+++ SRC/components/anajulia/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function anajulia(){
+    return(
+       <View>
+        <Text>Ana Julia Ferreira dos Santos 3B1</Text>
+       </View>
+
+    );
+}
---
+++ SRC/components/carol/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function carol(){
+    return(
+       <View>
+        <Text>Ana Carolina Martins Correa 3B1</Text>
+       </View>
+
+    );
+}
